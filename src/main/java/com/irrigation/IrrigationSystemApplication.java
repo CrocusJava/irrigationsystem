d0060ee316c9353fe7355320a5757c75b3132256
Crocusjava <crocusjava@gmail.com>
2016-03-26 22:40:04 +0200

    [git] Added project files

---
+++ src/main/java/com/irrigation/IrrigationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.irrigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrrigationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrrigationSystemApplication.class, args);
+	}
+}
